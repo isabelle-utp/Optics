b98aa6c1bd7b11a0c6f6de1b61589f00fb148bcc
Simon Foster <simondf@gmail.com>
2026-09-22 16:45:29 +0100

    Compatibility fixes for Isabelle2025-2

--- Scenes.thy
+++ Scenes.thy
@@ -51,8 +51,7 @@ is "\<lambda> F. {(s\<^sub>1, s\<^sub>2). (\<forall> s. F s\<^sub>1 s = F s\<^su
 lemma equiv_region: "equiv UNIV (region X)"
   apply (transfer)
   apply (rule equivI)
-    apply (rule refl_onI)
-     apply (auto)
+     apply (auto simp add: reflI)
    apply (rule symI)
    apply (auto)
   apply (rule transI)
@@ -62,8 +61,7 @@ lemma equiv_region: "equiv UNIV (region X)"
 lemma equiv_coregion: "equiv UNIV (coregion X)"
   apply (transfer)
   apply (rule equivI)
-    apply (rule refl_onI)
-     apply (auto)
+     apply (auto simp add: reflI)
    apply (rule symI)
    apply (auto)
   apply (rule transI)
